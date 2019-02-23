47737e15564949bd755588bfb4d9d9d06c68b68c
archit_chandra <send2archit.chandra@gmail.com>
2019-02-23 23:48:12 +0100

    get all employees from database

--- 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
+++ 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
@@ -1,38 +1,31 @@
 package com.example.crudmvc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.crudmvc.entity.Employee;
+import com.example.crudmvc.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> employees;
+    private EmployeeService employeeService;
 
-    // load employees data
-    @PostConstruct
-    private void loadData() {
-        Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
-        Employee emp2 = new Employee(2, "Archit", "Chandra", "archit@gmail.com");
-        Employee emp3 = new Employee(3, "Trisha", "Sharma", "trisha@gmail.com");
-        employees = new ArrayList<>();
-        employees.add(emp1);
-        employees.add(emp2);
-        employees.add(emp3);
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     // add mapping for "/list"
     @GetMapping("/list")
     public String getAllEmployess(Model model) {
+        List<Employee> employees = employeeService.getAllEmployees();
         model.addAttribute("employees", employees);
         return "list-employees";
     }
