4230d53749e3a57dfb06d54c4f1ad4a1876082c7
archit_chandra <send2archit.chandra@gmail.com>
2019-02-25 00:35:55 +0100

    add a employee: added controller method to obtain employee from "employee-form"

--- 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
+++ 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.crudmvc.entity.Employee;
@@ -35,5 +37,11 @@ public class EmployeeController {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "/employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/employees/list";
     }
 }
