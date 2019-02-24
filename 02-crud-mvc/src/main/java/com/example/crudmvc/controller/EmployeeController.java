4db3251e5fbac8863ff3efac1a12506a466bcd72
archit_chandra <send2archit.chandra@gmail.com>
2019-02-25 00:28:04 +0100

    add a employee: added controller method to show "employee-form" to add an employee

--- 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
+++ 02-crud-mvc/src/main/java/com/example/crudmvc/controller/EmployeeController.java
@@ -29,4 +29,11 @@ public class EmployeeController {
         model.addAttribute("employees", employees);
         return "/employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "/employees/employee-form";
+    }
 }
